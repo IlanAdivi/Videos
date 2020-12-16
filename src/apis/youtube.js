bfd83fa9297b88624fb224beab7e4ace87764a95
Ilan Adivi <ilanadivi47@gmail.com>
2020-12-16 17:56:00 +0200

    Enabling netlify

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
 
-const KEY = `AIzaSyBhA79ZVa9EwMVMlGzRP41Jmz9GeuxzByk`;
-
 export default axios.create({
     baseURL: `https://www.googleapis.com/youtube/v3`,
     params: {
         part: 'snippet',
         type: 'video',
         maxResults: 5,
-        key: `${KEY}`
+        key: `${process.env.REACT_APP_YOUTUBE_API_KEY}`
     }
 })
